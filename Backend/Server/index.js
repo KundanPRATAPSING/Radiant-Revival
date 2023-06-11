727f2be365263af2ad3a7025184a67318ead057c
Ankit-Ransh <ankitanand2125@gmail.com>
2023-06-11 21:44:29 +0530

    Fixed 304 error in Login.js

--- Backend/Server/index.js
+++ Backend/Server/index.js
@@ -1,50 +1,83 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/customer');
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+    await mongoose.connect('mongodb://127.0.0.1:27017/customer');
 }
 
 const userSchema = new mongoose.Schema({
     name: String,
     username: String,
     address: String,
-    phoneNumber: String, // first two digit is country code for india
+    phoneNumber: String, // first two digits are the country code for India
     email: String,
     password: String,
     confirmPassword: String,
-})
+});
 
-const Users = new mongoose.model('Users', userSchema)
+const Users = new mongoose.model('Users', userSchema);
 
-const server = express()
+const server = express();
 
-server.use(cors())
-server.use(bodyParser.json())
+server.use(cors());
+server.use(bodyParser.json());
 
-server.post('/register', async (req,res) => {
+// CORS middleware
+server.use((req, res, next) => {
+    // Replace '*' with the appropriate origin(s) or configure it dynamically
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-    let user = new Users()
+    // Add other allowed methods as needed
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
 
-    user.name = req.body.name
-    user.username = req.body.username
-    user.address = req.body.address
-    user.phoneNumber = req.body.phoneNumber
-    user.email = req.body.email
-    user.password = req.body.password
-    user.confirmPassword = req.body.confirmPassword
-    await user.save()
+    // Add other allowed headers as needed
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    res.json(req.body)
-})
+    // Handle preflight request
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
 
+    // Set cache-control headers to prevent caching
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
 
-server.listen('8080',(req,res) =>{
-    console.log('Server Connected')
-})
+    next();
+});
+
+server.post('/register', async (req, res, next) => {
+    let user = new Users();
+
+    user.name = req.body.name;
+    user.username = req.body.username;
+    user.address = req.body.address;
+    user.phoneNumber = req.body.phoneNumber;
+    user.email = req.body.email;
+    user.password = req.body.password;
+    user.confirmPassword = req.body.confirmPassword;
+    
+    try {
+        await user.save();
+        res.json(req.body);
+    } catch (error) {
+        res.status(500).json({ error: 'Could not register user' });
+    }
+});
+
+server.post('/login', (req, res) => {
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+
+    console.log(req.body);
+    res.json(req.body);
+});
+
+server.listen(8080, () => {
+    console.log('Server Connected');
+});
--- Frontend/e-commerce-app/src/Components/Login.js
+++ Frontend/e-commerce-app/src/Components/Login.js
@@ -1,138 +1,222 @@
-import React, { useState } from "react";
-import "./Login.css";
-import { Link ,useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import "./css/Login.css";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Login() {
-
     const location = useLocation();
     const isFromSignUp = location.state && location.state.fromSignUp;
-    const [loginUser,setLoginUser] = useState({
-        'email': "",
-        'password': "",
-    })
+    const [loginUser, setLoginUser] = useState({
+        email: "",
+        password: "",
+    });
+    const [loginErrors, setLoginErrors] = useState({});
+    const [isUser, setIsUser] = useState(false);
 
-    function handleChange(e){
-        const {name,value} = e.target;
+    function handleChange(e) {
+        const { name, value } = e.target;
         setLoginUser({
             ...loginUser,
-            [name]: value
-        })
+            [name]: value,
+        });
     }
 
-    function handleSubmit(e){
+    async function handleSubmit(e) {
         e.preventDefault();
-        console.log({loginUser})
+        setLoginErrors(validateLogin(loginUser));
+
+        if (Object.keys(loginErrors).length === 0) {
+            const timestamp = Date.now();
+            const url = `http://localhost:8080/login?timestamp=${timestamp}`;
+
+            try {
+                const response = await fetch(url, {
+                    method: "POST",
+                    body: JSON.stringify(loginUser),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+
+                if (response.ok) {
+                    const userData = await response.json();
+                    console.log(userData);
+                } else if (response.status === 304) {
+                    console.log("Retrying login...");
+                    await handleSubmit(e); // Recursive call to handleSubmit function
+                } else {
+                    // Handle server-side error
+                    throw new Error("Server error");
+                }
+            } catch (error) {
+                // Handle network error or server-side error
+                console.log("Error:", error.message);
+            }
+        }
     }
 
-  return (
-    <>
 
-      {/* Signup container  */}
-      <div className="LoginPageContainer">
-        <div className="LoginPageInnerContainer">
-          <div className="ImageContianer">
-            <img
-              src={require("./images/login_left.png")}
-              alt="Login left"
-              className="GroupImage"
-            />
-          </div>
-          <div className="LoginFormContainer">
-            <div className="LoginFormInnerContainer">
-              <div className="LogoContainer">
-                <img
-                  src={require("./images/login_logo.png")}
-                  className="logo"
-                  alt="login-logo"
-                />
-              </div>
-              <header className="header">Login </header>
-              <header className="subHeader">
-                Welcome to <b>The Inventory Emporium!</b> Please Enter your
-                Details
-              </header>
+    useEffect(() => {
+        const handleBeforeUnload = (event) => {
+            if (Object.keys(loginErrors).length > 0) {
+                event.preventDefault();
+                event.returnValue = ""; // Required for Chrome and legacy browsers
+            }
+        };
 
-              {isFromSignUp && <div className="alert alert-info" role="alert">
-                  Account Created Successfully
+        window.addEventListener("beforeunload", handleBeforeUnload);
+
+        return () => {
+            window.removeEventListener("beforeunload", handleBeforeUnload);
+        };
+    }, [loginErrors]);
+
+
+    useEffect(() => {
+        if (Object.keys(loginErrors).length === 0 && isUser) {
+            console.log(loginUser);
+        }
+    }, [loginErrors, isUser, loginUser]);
+
+    function validateLogin(loginUser) {
+        const errors = {};
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+        if (loginUser.email === "") {
+            errors.email = "Email is required!";
+        } else if (!regex.test(loginUser.email)) {
+            errors.email = "This is not a valid email format!";
+        }
+
+        if (loginUser.password === "") {
+            errors.password = "Password is required!";
+        }
+
+        return errors;
+    }
+
+    return (
+        <>
+            {/* Signup container  */}
+            <div className="LoginPageContainer">
+                <div className="LoginPageInnerContainer">
+                    <div className="ImageContianer">
+                        <img
+                            src={require("./images/login_left.png")}
+                            alt="Login left"
+                            className="GroupImage"
+                        />
+                    </div>
+                    <div className="LoginFormContainer">
+                        <div className="LoginFormInnerContainer">
+                            <div className="LogoContainer">
+                                <img
+                                    src={require("./images/login_logo.png")}
+                                    className="logo"
+                                    alt="login-logo"
+                                />
+                            </div>
+                            <header className="header">Login </header>
+                            <header className="subHeader">
+                                Welcome to <b>The Inventory Emporium!</b> Please Enter your
+                                Details
+                            </header>
+
+                            {isFromSignUp && (
+                                <div className="alert alert-info" role="alert">
+                                    Account Created Successfully
+                                </div>
+                            )}
+
+                            <form onSubmit={handleSubmit}>
+                                {/* <!-- Email  --> */}
+                                <div className="inputContainer">
+                                    <label className="label" htmlFor="loginEmail">
+                                        <img
+                                            src={require("./images/login_email.png")}
+                                            className="labelIcon"
+                                            alt="email-logo"
+                                        />
+                                        <span>Email Address*</span>
+                                    </label>
+                                    <input
+                                        type="email"
+                                        className="input"
+                                        id="loginEmail"
+                                        placeholder="Enter your Email Address"
+                                        name="email"
+                                        value={loginUser.email}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+
+                                {/* Display Error Message  */}
+                                {loginErrors.email && (
+                                    <div className="alert alert-danger" role="alert">
+                                        {loginErrors.email}
+                                    </div>
+                                )}
+
+                                {/* <!-- Password --> */}
+                                <div className="inputContainer">
+                                    <label className="label" htmlFor="loginPassword">
+                                        <img
+                                            src={require("./images/login_pass.png")}
+                                            className="labelIcon"
+                                            alt="password-logo"
+                                        />
+                                        <span>Password*</span>
+                                    </label>
+                                    <input
+                                        type="password"
+                                        className="input"
+                                        id="loginPassword"
+                                        placeholder="Enter your Password"
+                                        name="password"
+                                        value={loginUser.password}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+
+                                {/* Display Error Message  */}
+                                {loginErrors.password && (
+                                    <div className="alert alert-danger" role="alert">
+                                        {loginErrors.password}
+                                    </div>
+                                )}
+
+                                {/* <!-- show password --> */}
+                                <div className="OptionsContainer">
+                                    <div className="checkboxContainer">
+                                        <input
+                                            type="checkbox"
+                                            id="login_show_password"
+                                            className="checkbox"
+                                        />
+                                        <label htmlFor="login_show_password">Show Password</label>
+                                    </div>
+                                </div>
+
+                                <button className="LoginButton" type="submit">
+                                    Login
+                                </button>
+                                <header className="subHeader">
+                                    <p>
+                                        Don't have an account
+                                        <Link
+                                            className="btn btn-outline-dark mx-2 my-2"
+                                            to="/"
+                                            role="button"
+                                            id="login_button"
+                                        >
+                                            Sign Up
+                                        </Link>
+                                    </p>
+                                </header>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                }
-
-              <form onSubmit = {handleSubmit}>
-                {/* <!-- Email  --> */}
-                <div className="inputContainer">
-                  <label className="label" htmlFor="loginEmail">
-                    <img
-                      src={require("./images/login_email.png")}
-                      className="labelIcon"
-                      alt="email-logo"
-                    />
-                    <span>Email Address*</span>
-                  </label>
-                  <input
-                    type="email"
-                    className="input"
-                    id="loginEmail"
-                    placeholder="Enter your Email Address"
-                    name="email"
-                    value={loginUser.email}
-                    onChange={handleChange}
-                  />
-                </div>
-
-                {/* <!-- Password --> */}
-                <div className="inputContainer">
-                  <label className="label" htmlFor="loginPassword">
-                    <img
-                      src={require("./images/login_pass.png")}
-                      className="labelIcon"
-                      alt="password-logo"
-                    />
-                    <span>Password*</span>
-                  </label>
-                  <input
-                    type="password"
-                    className="input"
-                    id="loginPassword"
-                    placeholder="Enter your Password"
-                    name="password"
-                    value={loginUser.password}
-                    onChange={handleChange}
-                  />
-                </div>
-
-                {/* <!-- show password --> */}
-                <div className="OptionsContainer">
-                  <div className="checkboxContainer">
-                    <input
-                      type="checkbox"
-                      id="login_show_password"
-                      className="checkbox"
-                    />
-                    <label htmlFor="login_show_password">Show Password</label>
-                  </div>
-                </div>
-
-                <button className="LoginButton" type="submit">
-                  Login
-                </button>
-                <header className="subHeader">
-                  <p>
-                    Don't have an account
-                    <Link
-                      className="btn btn-outline-dark mx-2 my-2"
-                      to="/"
-                      role="button"
-                      id="login_button"
-                    >
-                      Sign Up
-                    </Link>
-                  </p>
-                </header>
-              </form>
             </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
